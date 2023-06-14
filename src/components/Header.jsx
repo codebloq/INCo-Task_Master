7b84bd844251de8339e3f52f757f119056b0dcf7
Himanshu <66199506+codebloq@users.noreply.github.com>
2023-06-15 04:58:18 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AssignmentIcon />
+        Task Master
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
